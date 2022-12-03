0e27652740d5ded6428d649259a43a33efd2c644
Howard Bussey <howard.bussey@gmail.com>
2022-12-03 08:43:40 -0500

    2.2 done

--- 02/aoc.ex
+++ 02/aoc.ex
@@ -18,19 +18,25 @@ defmodule Aoc do
     d
     |> process_data
     |> print_answer
+    d |> process_data_2 |> print_answer
   end
 
   def read_data do
     File.read!("data.txt")
-   end
+  end
 
   def process_data(d) do
     d
     |> String.split("\n")
-    # |> IO.inspect(label: "first split")
     |> Enum.map(fn a -> String.split(a) end)
-    # |> IO.inspect(label: "2nd split")
- end
+  end
+
+ def process_data_2(d) do
+    process_data(d)
+    # |> Enum.take(5) |> IO.inspect(label: "process_data_2 A")
+    |> Enum.map(fn [e, r] -> [e, shape_for(e,r)] end)
+    # |> IO.inspect(label: "process_data_2 B")
+  end
 
   def print_answer(d) do
     d
@@ -64,5 +70,24 @@ defmodule Aoc do
 
     # IO.inspect({e, m, rv}, label: "a Score")
     rv
+  end
+
+  def shape_for(e,r) do
+    # A, B, C = ROCK, PAPER, SCISSORS
+    # Incoming: X, Y,  Z = LOSE, DRAW, WIN
+    # result: X, Y,  Z = ROCK, PAPER, SCISSORS
+    # ROCK: 1, PAPER: 2, SCISSORS: 3
+    # LOSE: 0, DRAW: 3, WIN: 6
+    case {e, r} do
+      {"A", "X"} -> "Z"
+      {"A", "Y"} -> "X"
+      {"A", "Z"} -> "Y"
+      {"B", "X"} -> "X"
+      {"B", "Y"} -> "Y"
+      {"B", "Z"} -> "Z"
+      {"C", "X"} -> "Y"
+      {"C", "Y"} -> "Z"
+      {"C", "Z"} -> "X"
+    end
   end
 end
